fb418114ee475e30ea6c86586905a463fdc943ce
SamGiles <aphro.cag@googlemail.com>
2013-06-12 13:51:03 +0100

    Mac traffic shaper

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Packet Shaper
+
+Shapes packets on Mac OS X, useful for debugging network applications under different network conditions.
+
+## Usage
+
+```SHELL
+shaper [-h] -d DELAY -bw BANDWIDTH -pl PACKET_LOSS
+arguments:
+  -h, --help       show this help message and exit
+  -d DELAY         The network propagation delay
+  -bw BANDWIDTH    The up and down bandwidth in KBit/s
+  -pl PACKET_LOSS  The percentage of packets lost
+```
---
+++ shaper.py
@@ -0,0 +1,47 @@
+﻿
+ # Copyright (c) 2013 Samuel Giles
+
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+
+import signal
+import sys
+import argparse
+from subprocess import call
+from pprint import pprint
+
+parser = argparse.ArgumentParser(prog='shaper',description='Shape Mac OS X network traffic.')
+parser.add_argument('-d', dest='delay', type=int, help='The propagation delay', required=True)
+parser.add_argument('-bw', dest='bandwidth', type=int, help='The up and down bandwidth in KBit/s', required=True)
+parser.add_argument('-pl', dest='packet_loss', type=int, help='The percentage of packets lost', required=True)
+
+args = parser.parse_args()
+
+
+def signal_handler(signal, frame):
+        print 'Restoring network'
+        call(["ipfw", "delete", "1"])
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+call(["ipfw", "pipe", "1", "config", "bw", str(args.bandwidth) + "Kbit/s", "delay", str(args.delay), "plr", str(args.packet_loss / 100)])
+call(["ipfw", "add", "1", "pipe", "1", "ip", "from", "any", "to", "any"])
+
+print 'Press Ctrl+C to restore'
+signal.pause()
